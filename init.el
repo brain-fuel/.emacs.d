db6510e0a831fd84150e18ee9074626ed0e3df79
Matt Laine <matt@brain-fuel.co>
2022-08-26 15:55:39 -0400

    Revert "literate config only"
    This reverts commit 88b93790a2ce7d17b2fd88e856792ecf6f174d1f.

--- init.el
+++ init.el
@@ -1,1 +1,56 @@
-(org-babel-load-file "~/.emacs.d/init.org")
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)                     ; Disable visible scroll bar
+(tool-bar-mode -1)                       ; Disable the toolbar
+(tooltip-mode -1)                        ; Disable tooltips
+(set-fringe-mode 10)                     ; Give some "breathing room"
+(global-display-line-numbers-mode)       ; Display line numbers everywhere
+(global-set-key
+ (kbd "<escape>") 'keyboard-escape-quit) ; Make ESC quit prompts
+
+(set-face-attribute
+ 'default nil
+ :font "Monoid"
+ :height 156)
+
+(load-theme 'wombat)
+
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+	("org"   . "https://orgmode.org/elpa/")
+	("elpa"  . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package command-log-mode)
+
+(use-package ivy
+  :bind (; TODO Install Swiper ("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-alt-done)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
